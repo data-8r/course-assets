b02007944c98a830b9943c9716eb74c67c15f9f8
Joseph Simonian <j.simonian10@gmail.com>
2017-06-28 18:59:57 -0700

    su17/hw/hw01.push

---
+++ materials/su17/hw/hw01/tests/q4.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 4_',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> Lebron_height_in_inches == 80
+          True
+          >>> Lebron_height_in_meters == 2.032
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
