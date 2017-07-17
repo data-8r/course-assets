5fecab4aee529920932fe3f74ba32e055f298170
austenzhu <austenzhu@berkeley.edu>
2017-07-17 02:19:37 -0700

    su17/lab/lab09.push

---
+++ materials/su17/lab/lab09/tests/q3.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> shortest_twin_height == 63
+          True
+          >>> tallest_twin_height == 71
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
