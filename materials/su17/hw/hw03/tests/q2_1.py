e2faa50d46d36dfd3c072198cededc219c1186c3
austenzhu <austenzhu@berkeley.edu>
2017-07-17 02:10:41 -0700

    su17/hw/hw03.push

---
+++ materials/su17/hw/hw03/tests/q2_1.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 2_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> last_three_rows
+          Person
+          Jonathan
+          Joseph
+          Maya""",
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
