630591b9a261896796922896825e8db333e41c3a
Joseph Simonian <j.simonian10@gmail.com>
2017-07-20 13:35:32 -0700

    su17/hw/hw04.push

---
+++ materials/su17/hw/hw04/tests/q3_6.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 3_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> closest_station_name
+          'Oakland'
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
