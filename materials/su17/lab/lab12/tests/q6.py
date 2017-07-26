5f4c63335e28ec6e1aa5e76f40228e279ac67d77
austenzhu <austenzhu@berkeley.edu>
2017-07-26 09:29:45 -0700

    su17/lab/lab12.push

---
+++ materials/su17/lab/lab12/tests/q6.py
@@ -0,0 +1,20 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> int(avg_water_usage)
+          100
+          """
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
