cbbd52e1969c6015aa2000022b6747ae405f2e89
austenzhu <austenzhu@berkeley.edu>
2017-07-27 19:48:40 -0700

    su17/hw/hw05.push

---
+++ materials/su17/hw/hw05/tests/q2_1.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question 2_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> print("dummy test to satisfy okpy") 
+          dummy test to satisfy okpy
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
