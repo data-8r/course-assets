029f96aab9ffe23bfcd38b5ddfc93a12467a6ea9
hmilner <hmilner@conviva.com>
2017-07-31 10:36:43 -0700

    su17/lab/lab15.push

---
+++ materials/su17/lab/lab15/tests/q6.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> test_statistic_function = lambda tbl: np.mean(tbl.column("Interested in raising animals"))
+          >>> abs(sample_once(649) - 0.91) < 0.02
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/su17/lab/lab15/tests/q7.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> abs(np.round(test_statistic_function(farming),3) - 0.918) < 0.005
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
